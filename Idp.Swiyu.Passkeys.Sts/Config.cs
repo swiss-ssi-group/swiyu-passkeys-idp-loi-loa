3269a78912793006aeeb44f54b114b7b4fc05ff7
damienbod <damien_bod@hotmail.com>
2025-12-21 22:30:46 +0100

    Clean up

--- Idp.Swiyu.Passkeys.Sts/Config.cs
+++ Idp.Swiyu.Passkeys.Sts/Config.cs
@@ -8,18 +8,15 @@ namespace Idp.Swiyu.Passkeys.Sts;
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        {
+        [
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
-        };
+        ];
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-        {
-            new ApiScope("scope1"),
+        [
             new ApiScope("scope2"),
-        };
+        ];
 
     public static IEnumerable<Client> Clients =>
         [
