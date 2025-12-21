f4bffc03f75cf1e2c9804146cd52bdc103c7b64b
damienbod <damien_bod@hotmail.com>
2025-12-21 18:30:42 +0100

    fixing

--- Idp.Swiyu.Passkeys.Web/Program.cs
+++ Idp.Swiyu.Passkeys.Web/Program.cs
@@ -1,5 +1,6 @@
 using Idp.Swiyu.Passkeys.Web;
 using Idp.Swiyu.Passkeys.Web.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,38 @@ builder.AddRedisOutputCache("cache");
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpContextAccessor();
+
+var oidcConfig = builder.Configuration.GetSection("OpenIDConnectSettings");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    options.DefaultSignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.Cookie.Name = "__Host-idp-swiyu-passkeys-web";
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    // can be strict if same-site
+    //options.Cookie.SameSite = SameSiteMode.Strict;
+})
+.AddOpenIdConnect(options =>
+{
+    builder.Configuration.GetSection("OpenIDConnectSettings").Bind(options);
+
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.ResponseType = OpenIdConnectResponseType.Code;
+
+    options.SaveTokens = true;
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = "name"
+    };
+});
 
 builder.Services.AddHttpClient<WeatherApiClient>(client =>
     {
