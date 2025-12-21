33190b01fedb2ec5fd26179bcae57637786a4e2c
damienbod <damien_bod@hotmail.com>
2025-12-21 17:54:51 +0100

    Init duende IDP

---
+++ Idp.Swiyu.Passkeys.Sts/HostingExtensions.cs
@@ -0,0 +1,142 @@
+// Copyright (c) Duende Software. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Globalization;
+using Duende.IdentityServer;
+using IdentityServerAspNetIdentityPasskeys.Data;
+using IdentityServerAspNetIdentityPasskeys.Models;
+using IdentityServerAspNetIdentityPasskeys.Passkeys;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using Serilog.Filters;
+
+namespace IdentityServerAspNetIdentityPasskeys;
+
+internal static class HostingExtensions
+{
+    public static WebApplicationBuilder ConfigureLogging(this WebApplicationBuilder builder)
+    {
+        // Write most logs to the console but diagnostic data to a file.
+        // See https://docs.duendesoftware.com/identityserver/diagnostics/data
+        builder.Host.UseSerilog((ctx, lc) =>
+        {
+            lc.WriteTo.Logger(consoleLogger =>
+            {
+                consoleLogger.WriteTo.Console(
+                    outputTemplate:
+                    "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                    formatProvider: CultureInfo.InvariantCulture);
+                if (builder.Environment.IsDevelopment())
+                {
+                    consoleLogger.Filter.ByExcluding(Matching.FromSource("Duende.IdentityServer.Diagnostics.Summary"));
+                }
+            });
+            if (builder.Environment.IsDevelopment())
+            {
+                lc.WriteTo.Logger(fileLogger =>
+                {
+                    fileLogger
+                        .WriteTo.File("./diagnostics/diagnostic.log", rollingInterval: RollingInterval.Day,
+                            fileSizeLimitBytes: 1024 * 1024 * 10, // 10 MB
+                            rollOnFileSizeLimit: true,
+                            outputTemplate:
+                            "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                            formatProvider: CultureInfo.InvariantCulture)
+                        .Filter
+                        .ByIncludingOnly(Matching.FromSource("Duende.IdentityServer.Diagnostics.Summary"));
+                }).Enrich.FromLogContext().ReadFrom.Configuration(ctx.Configuration);
+            }
+        });
+        return builder;
+    }
+
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.Configure<IdentityPasskeyOptions>(options =>
+            {
+                // Allow https://localhost:5001 origin.
+                options.ValidateOrigin = context => ValueTask.FromResult(
+                    context.Origin == "https://localhost:5001");
+            });
+        }
+
+        builder.Services
+            .AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // Use a large chunk size for diagnostic data in development where it will be redirected to a local file.
+                if (builder.Environment.IsDevelopment())
+                {
+                    options.Diagnostics.ChunkSize = 1024 * 1024 * 10; // 10 MB
+                }
+            })
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients)
+            .AddAspNetIdentity<ApplicationUser>()
+            .AddLicenseSummary();
+
+        builder.Services.AddAuthentication()
+            .AddOpenIdConnect("oidc", "Sign-in with demo.duendesoftware.com", options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+                options.SaveTokens = true;
+
+                options.Authority = "https://demo.duendesoftware.com";
+                options.ClientId = "interactive.confidential";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name",
+                    RoleClaimType = "role"
+                };
+            });
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseIdentityServer();
+        app.UseAuthorization();
+
+        app.MapPasskeyEndpoints();
+
+        app.MapRazorPages()
+            .RequireAuthorization();
+
+        return app;
+    }
+}
