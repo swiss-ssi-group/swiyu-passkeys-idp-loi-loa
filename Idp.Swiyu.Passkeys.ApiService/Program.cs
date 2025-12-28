738627588637d977d108a5ce0c46ed8dd4353b26
damienbod <damien_bod@hotmail.com>
2025-12-28 20:22:13 +0100

    clean up

--- Idp.Swiyu.Passkeys.ApiService/Program.cs
+++ Idp.Swiyu.Passkeys.ApiService/Program.cs
@@ -8,12 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
 
-// Add services to the container.
 builder.Services.AddProblemDetails();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer(options =>
