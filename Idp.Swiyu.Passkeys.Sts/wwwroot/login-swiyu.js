53bd2b7fccd657b683e9725f6b064ece05f348ea
damienbod <damien_bod@hotmail.com>
2025-12-29 20:12:30 +0100

    Add UI scripts

---
+++ Idp.Swiyu.Passkeys.Sts/wwwroot/login-swiyu.js
@@ -0,0 +1,16 @@
+﻿
+const verificationId = document.getElementById('verificationId');
+const form = document.getElementById("pollSwiyu");
+const buttonPollSwiyu = document.getElementById("buttonPollSwiyu");
+
+if (verificationId != null) {
+
+    let checkStatus = setInterval(function () {
+        if (verificationId) {
+            if (form) {
+                form.submit();
+            }
+            clearInterval(checkStatus)
+        }
+    }, 1500);
+}
