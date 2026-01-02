878030d62383b112e6c76d535913fed6b1a94e1d
damienbod <damien_bod@hotmail.com>
2026-01-02 20:47:33 +0100

    Clean up

--- Idp.Swiyu.Passkeys.Sts/HostingExtensions.cs
+++ Idp.Swiyu.Passkeys.Sts/HostingExtensions.cs
@@ -66,7 +66,7 @@ internal static class HostingExtensions
         builder.Services.AddRazorPages();
 
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(builder.Configuration.GetConnectionString("database")));
 
         builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
             {
