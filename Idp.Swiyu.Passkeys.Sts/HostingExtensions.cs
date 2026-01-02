22dadb3473876af8e3fed9ea8f0b292a60ff2bd1
damienbod <damien_bod@hotmail.com>
2026-01-02 22:24:01 +0100

    clean up

--- Idp.Swiyu.Passkeys.Sts/HostingExtensions.cs
+++ Idp.Swiyu.Passkeys.Sts/HostingExtensions.cs
@@ -59,8 +59,7 @@ internal static class HostingExtensions
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
         builder.AddServiceDefaults();
-        //builder.AddApplicationDbContext("database");
-
+     
         builder.Services.AddScoped<VerificationService>();
 
         builder.Services.AddHttpClient();
