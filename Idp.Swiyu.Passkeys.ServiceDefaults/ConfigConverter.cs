2bcef24c3b6e6d6a9b29446b4f358b0b92bc0994
damienbod <damien_bod@hotmail.com>
2026-08-30 16:44:49 +0200

    Add Pem and base64 converters

---
+++ Idp.Swiyu.Passkeys.ServiceDefaults/ConfigConverter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace Idp.Swiyu.Passkeys.ServiceDefaults;
+
+public static class ConfigConverter
+{
+    public static string GetPemFromBase64Config(string config, IConfiguration configuration)
+    {
+        var base64String = configuration.GetValue<string>(config);
+
+        if (string.IsNullOrEmpty(base64String))
+        {
+            throw new ArgumentException($"PEM Configuration value for '{config}' is missing or empty.");
+        }
+
+        return Encoding.UTF8.GetString(Convert.FromBase64String(base64String));
+    }
+
+    public static string GetPemFromBase64(string base64String)
+    {
+        return Encoding.UTF8.GetString(Convert.FromBase64String(base64String));
+    }
+
+    public static string CreateBase64FromPem(string pem)
+    {
+        var base64String = Convert.ToBase64String(Encoding.UTF8.GetBytes(pem));
+
+        return base64String;
+    }
+}
